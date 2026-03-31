b2bcbd87370f17a880b7eb695733572268e72f2f
LorianaDIANO <128498370+LorianaD@users.noreply.github.com>
2026-03-31 19:16:55 +0200

    add: header code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Route, Routes } from 'react-router'
 import Home from './pages/Home'
+import MainLayout from './components/layout/MainLayout'
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route element={<MainLayout/>}>
+        <Route path='/' element={<Home/>}/>
+      </Route>
     </Routes>
   )
 }
